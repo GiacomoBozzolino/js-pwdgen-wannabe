0c7f5a15ef646dbcd73ba7c356f069f8104a67d1
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-05-16 16:09:51 +0200

    [FEAT] tolti console.log file js \ fine esercizio

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,14 +1,14 @@
 // PROMPT PER CHIEDERE NOME 
 let nome = prompt ("Qual è il tuo nome?")
-console.log(nome)
+
 // PROMPT PER CHIEDERE COGNOME 
 let cognome = prompt ("Qual è il tuo cognome?")
-console.log(cognome)
+
 // PROMPT PER CHIEDERE COLORE 
 let colore = prompt ("Qual è il tuo colore preferito?")
-console.log(colore)
+
 // concatenazione stringhe
 let password = nome + cognome + colore + 21 
-console.log(password)
+
 // STAMPA PASSWORD SULLA PAGINA
 document.getElementById("password").innerHTML= `La tua password è: ${password}`
